7b76382a851cd5a04b9b5e615d22d055cada4f10
Vaugert <lindstromfabian@gmail.com>
2022-07-01 17:01:17 +0000

    install summernote and create home screen view

--- CrispyFeed/urls.py
+++ CrispyFeed/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from feed import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_screen, name='home'),
+    path('summernote/', include('django_summernote.urls'))
+    
 ]
--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+    list_filter = ('title', 'created_date')
+    list_display = ('author', 'title', 'created_date')
+    search_fields = ('title', 'created_date')
+    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_date')
+
+
