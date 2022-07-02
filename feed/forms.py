01f1341ca1f08be03f5cc7b43a31310cff5f05aa
Vaugert <lindstromfabian@gmail.com>
2022-07-02 15:49:24 +0000

    create forms.py class CommentForm

---
+++ feed/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post, Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        fields = ('content',)
+        labels = {
+            'content': ('Leave a comment')
+        } 
+        model = Comment
+        widgets = {'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3})}
+    
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment
 
 
 def home_screen(request):
@@ -9,3 +9,20 @@ def home_screen(request):
         'posts': posts
         }
     return render(request, 'index.html', context)
+
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post=post) 
+
+
+def post_comment(request):
+    
+    if request.method == 'POST':
+
+        name = request..get('author')
+
+        return redirect ('home_screen')
+    return render(request, 'templates/post_details.html' )
+
