8a8d21a2443f1871be7d0c2bab00ca6ffd783422
Vaugert <lindstromfabian@gmail.com>
2022-07-03 15:07:37 +0000

    create navbar and install bootstrap

--- feed/views.py
+++ feed/views.py
@@ -7,6 +7,7 @@ def home_screen(request):
     posts = Post.objects.all()
     context = {
         'posts': posts
+
         }
     return render(request, 'index.html', context)
 
@@ -14,14 +15,14 @@ def home_screen(request):
 
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    comments = Comment.objects.filter(post=post) 
+    comments = Comment.objects.filter(post=post)
 
 
 def post_comment(request):
     
     if request.method == 'POST':
 
-        name = request..get('author')
+        
 
         return redirect ('home_screen')
     return render(request, 'templates/post_details.html' )
