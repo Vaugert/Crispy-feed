40c8a78d8ee6b49a199d38c6ebce99b13d8ee45a
Vaugert <lindstromfabian@gmail.com>
2022-07-05 16:39:01 +0000

    comment form views

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment
+from .forms import CommentForm
 
 
 def home_screen(request):
@@ -16,6 +17,19 @@ def home_screen(request):
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = Comment.objects.filter(post=post)
+
+    context = {
+        'post ': post,
+        'comments': comments,
+        'CommentForm': CommentForm
+        
+        
+
+
+    }
+
+
+    return render(request, 'post_details.html')
 
 
 def post_comment(request):
