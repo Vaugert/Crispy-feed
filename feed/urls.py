9f9e521af26ffff83ccaa189deadf15fb8864d49
Vaugert <lindstromfabian@gmail.com>
2022-07-07 18:45:02 +0000

    create layout on feed page,post details and create post

--- feed/urls.py
+++ feed/urls.py
@@ -4,9 +4,9 @@ from .models import Post
 
 urlpatterns = [
     path('post_details/<int:pk>', views.post_details, name='post_details'),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
     path('delete_post/<int:pk>', views.delete_post, name='delete_post'),
     path('edit_post/<int:pk>', views.edit_post, name='edit_post'),
-    path('create_post', views.create_post, name='delete_post')
+    path('create_post', views.create_post, name='create_post'),
 
 ]
